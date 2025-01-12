73b6a372e34cfbeeef65808b19dca72fac2f16fc
enekobarod <144792607+enekobarod@users.noreply.github.com>
2025-01-12 13:34:53 +0100

    "Unstaged changes"

--- BD/build/resources/main/strava_db.sql
+++ BD/build/resources/main/strava_db.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS strava_db;
 USE strava_db;
 
-
 DROP TABLE IF EXISTS reto_participantes;
 DROP TABLE IF EXISTS entrenamientos;
 DROP TABLE IF EXISTS retos;
@@ -25,31 +24,37 @@ CREATE TABLE usuarios (
 CREATE TABLE retos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     deporte VARCHAR(50) NOT NULL,
-    usuario_creador_id INT NOT NULL,
+    creador VARCHAR(50) NOT NULL,
     nombre VARCHAR(255) NOT NULL,
     fecha_inicio DATETIME NOT NULL,
     fecha_fin DATETIME NOT NULL,
     objetivo_distancia FLOAT,
     objetivo_tiempo FLOAT,
-    FOREIGN KEY (usuario_creador_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    FOREIGN KEY (creador) REFERENCES usuarios(username) ON DELETE CASCADE
 );
 
 CREATE TABLE entrenamientos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL,
+    usuario VARCHAR(50) NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     deporte VARCHAR(50) NOT NULL,
     distancia DOUBLE NOT NULL,
     duracion DOUBLE NOT NULL,
-    fecha_inicio DATETIME NOT NULL,
-    hora_inicio FLOAT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    fecha_inicio DATE NOT NULL,
+    hora_inicio TIME NOT NULL,
+    FOREIGN KEY (usuario) REFERENCES usuarios(username) ON DELETE CASCADE
 );
 
 CREATE TABLE reto_participantes (
-    reto_id INT NOT NULL,
-    usuario_id INT NOT NULL,
-    PRIMARY KEY (reto_id, usuario_id),
-    FOREIGN KEY (reto_id) REFERENCES retos(id) ON DELETE CASCADE,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+  id INT NOT NULL AUTO_INCREMENT,
+  usuario_id INT NOT NULL,
+  reto_id INT NOT NULL,
+  estado VARCHAR(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+  FOREIGN KEY (reto_id) REFERENCES retos(id) ON DELETE CASCADE
 );
+
+ALTER TABLE reto_participantes
+  ADD UNIQUE (usuario_id, reto_id);
+
